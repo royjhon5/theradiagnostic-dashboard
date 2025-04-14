e5f8caeb0392049c4e1d2e8353760f42fed0a1da
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2025-04-14 15:55:29 +0800

    update: latest update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
-      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-unused-vars": "off",
     },
   }),
 ];
