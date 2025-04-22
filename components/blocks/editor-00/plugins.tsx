8f0ab711baebf3c5c32a4dd9252cebfb0adeb09c
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2025-04-22 11:05:08 +0800

    update: updated plugins

--- components/blocks/editor-00/plugins.tsx
+++ components/blocks/editor-00/plugins.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
-import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@/components/editor/editor-ui/content-editable';
+import { useState } from "react";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@/components/editor/editor-ui/content-editable";
 
 export function Plugins() {
-  const [floatingAnchorElem, setFloatingAnchorElem] =
-    useState<HTMLDivElement | null>(null);
+  const [, setFloatingAnchorElem] = useState<HTMLDivElement | null>(null);
 
   const onRef = (_floatingAnchorElem: HTMLDivElement) => {
     if (_floatingAnchorElem !== null) {
@@ -21,7 +20,7 @@ export function Plugins() {
           contentEditable={
             <div className="">
               <div className="" ref={onRef}>
-                <ContentEditable placeholder={'Start typing ...'} />
+                <ContentEditable placeholder={"Start typing ..."} />
               </div>
             </div>
           }
